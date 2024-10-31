b1dc7e32658b48ce8aac25aa99fc6e6b77e61b2c
Joao Brosselin <166879336+sfbrosselin@users.noreply.github.com>
2024-10-30 23:40:12 -0300

    backend

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -9,7 +9,24 @@ const jwt = require('jsonwebtoken')
 app.use(cors())
 app.use(express.json())
 
+// cadastro
 
+app.post('/signup', async (req, res) => {
+    const {name, email, password} = req.body
+    const salt =  await bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+    try{
+        const signup = await pool.query('INSERT INTO users (user_email, user_name, user_password) VALUES($1, $2, $3)',[email, name, hashedPassword])
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+
+        res.json({ email, token, name })
+    }catch(err){
+        console.error(err)
+        if(err) {
+            res.json({ detail: err.detail})
+        }
+    }
+})
 
 
 app.listen(PORT, () => {
--- src/Frontend/ecorota/src/pages/Login.js
+++ src/Frontend/ecorota/src/pages/Login.js
@@ -14,33 +14,34 @@ const Main = styled.div`
 const LoginContainer = styled.form`
     display:flex;
     flex-direction:column;
+    justify-content: center;
+    align-items: center;
     background-color:#394130;
     width:45vw;
     height:85vh;
-    border-radius: 35% 10% 35% 10%;
+    border-radius: 100% 100% 100% 100%;
     font-family: "Baumans", system-ui;
 
     p{
         font-family:"Dosis", system-ui;
         font-weight: 600;
         font-size:2.75vh;
-        margin-left:10%;
+        width: 35%;
+        text-align: center;
+        align-self:center;
         color:#fff6de;
     }
 `
 const UserEmail = styled.input`
     width:70%;
-    margin-top:5%;
-    margin-left:15%;
     font-size:3.5vh;
     background-color:#acad94;
     border-radius: 1.5vh 1vh 2vh 1vh;
     font-family: "Baumans", system-ui;
 `
 const UserPassword = styled.input`
-    margin-top:6%;
     width:70%;
-    margin-left:15%;
+    margin-top:5%;
     font-size:3.5vh;
     background-color:#acad94;
     border-radius: 1.5vh 1vh 2vh 1vh;
@@ -68,6 +69,7 @@ const ConfirmationLink = styled(Link)`
     text-decoration:none;
     color:#907359;
     cursor: pointer;
+    overflow: break;
     &:hover{
         color:#acad95;
     }
@@ -85,31 +87,80 @@ const Logo = styled(Link)`
     }
 
 `
-
-
+const SignUpText = styled.h1`
+    color:#acad94;
+    margin-left:50%;
+    font-size: 7.5vh;
+    text-align:center;
+    margin:0;
+    margin-bottom:4%;
+    margin-top: 15%;
+    `
+const FirstName = styled.input`
+    width:70%;
+    margin-bottom:5%;
+    font-size:3.5vh;
+    background-color:#acad94;
+    border-radius: 1.5vh 1vh 2vh 1vh;
+    font-family: "Baumans", system-ui;
+`
 const Login = () =>{
 
     const [cookies, setCookie, removeCookie] = useCookies(null)
     const [email, setEmail] = useState(null)
+    const [name, setName] = useState(null)
     const [password, setPassword] = useState(null)
+    const [isLogin, setIsLogin] = useState(false)
     const [error, setError] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState(null)
 
     console.log(cookies)
 
-    const handleSubmit = async (e, endpoint) =>{
-        e.preventDefault()
-        
+    const viewLogin = (status) => {
+      setError(null)
+      setIsLogin(status)
+    }
+
+    const handleSubmit = async (e, endpoint) => {
+      e.preventDefault()
+      if (!isLogin && password !== confirmPassword) {
+        setError('Make sure passwords match!')
+        console.log('Make sure passwords match!')
+        return
+      }
+
+      const response = await fetch (`http://localhost:8001/signup`,{
+        method:'POST',
+        headers: { 'Content-Type' : 'application/json'},
+        body: JSON.stringify({ email, password, name })
+      })
+      
+      const data = await response.json()
+      if (data.detail) {
+        setError(data.detail)     
+      } 
+      else{
+          setCookie('Email', data.email)
+          setCookie('Name', data.name)
+          setCookie('AuthToken', data.token)
+
+          window.location.reload()
+      }
+
+      
     }
 
     return(
         <Main>
             <LoginContainer>
-                <Logo to = "/" className="fa-solid fa-route"></Logo>
-                <UserEmail type="email" placeholder="EMAIL"/>
-                <UserPassword type="password" placeholder="SENHA"/>
-                <SignUpButton type="submit" value="LOGIN"/>
-                <p>Ainda não tem uma conta? Cadastre-se <ConfirmationLink to = "/login">aqui!</ConfirmationLink></p>
+                {isLogin ? <Logo to = "/" className="fa-solid fa-route"></Logo> : <SignUpText>CADASTRO</SignUpText> }
+                {!isLogin && <FirstName type="name" placeholder="Nome" onChange={(e) => setName(e.target.value)}/>}
+                <UserEmail type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <UserPassword type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)}/> 
+                {!isLogin && <UserPassword type="password" placeholder="Confirmar senha" onChange={(e) => setConfirmPassword(e.target.value)}/>}
+                {isLogin ? <SignUpButton type="submit" value="LOGIN" onClick={(e) => handleSubmit(e,'login')}/> : <SignUpButton type="submit" value="CADASTRE-SE" onClick={(e) => handleSubmit(e,'signup')}/>}
             </LoginContainer>
+            {isLogin ? <p>Ainda não tem uma conta? Cadastre-se <ConfirmationLink to = "/signup">aqui!</ConfirmationLink></p> : <p>Ja esta cadastrado? Clique <ConfirmationLink to = "/login">aqui!</ConfirmationLink></p>}
         </Main>
         )
 }
